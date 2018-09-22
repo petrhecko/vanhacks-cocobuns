c4169b19ffd3097a61145400e5e6944be69f1ff3
Jim <jquitevis@xmatters.com>
2018-09-22 09:19:12 -0700

    PG Bootstrap

--- backend/src/main/java/com/swiftavenue/vanhacks2018/Vanhacks2018Application.java
+++ backend/src/main/java/com/swiftavenue/vanhacks2018/Vanhacks2018Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Vanhacks2018Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Vanhacks2018Application.class, args);
 	}
